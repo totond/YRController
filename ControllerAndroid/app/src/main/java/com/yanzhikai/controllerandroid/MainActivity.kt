4a23d40718f61668c70c480c37000e42a775547f
totond <yanzhikai_yjk@qq.com>
2019-02-24 23:18:53 +0800

    finish command UI

--- ControllerAndroid/app/src/main/java/com/yanzhikai/controllerandroid/MainActivity.kt
+++ ControllerAndroid/app/src/main/java/com/yanzhikai/controllerandroid/MainActivity.kt
@@ -2,11 +2,17 @@ package com.yanzhikai.controllerandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_control.setOnClickListener { jumpToControl() }
     }
+
+    private fun jumpToControl(){
+        ActivityJumpUtil.jumpToActivity(this,ControllerActivity::class.java)
+    }
+
 }
